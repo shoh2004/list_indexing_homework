fe5af0e6acce210a1172775ce47eb59262fd10ca
shohrux <shohruxergashev67@gmail.com>
2022-07-13 16:15:56 +0500

    list

--- bin/list_index10.dart
+++ bin/list_index10.dart
@@ -6,4 +6,23 @@
     Returns:
         int: return answer
 */
-void main() {}
+List func(list_num){
+    int i = 0;
+    List x = [];
+    if(list_num.first < list_num.last){
+        i = list_num.last;
+    }else{
+        i = list_num.first;
+        x.add(i);
+    }
+    return x;
+}
+void main() {
+    print(func([6,2,3,4,5]));
+}
+
+ 
+
+
+
+
