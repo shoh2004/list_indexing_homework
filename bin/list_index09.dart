af57cc490a8dacfac55d9a9220749da258c36710
shohrux <shohruxergashev67@gmail.com>
2022-07-13 11:31:05 +0500

    list

--- bin/list_index09.dart
+++ bin/list_index09.dart
@@ -6,4 +6,23 @@
     Returns:
         bool: return answer
 */
-void main() {}
+bool func(list1) {
+  int q = 0;
+  bool w = true;
+  List y = [];
+  while (q <= list1.length - 1) {
+    if (list1[q] == list1[list1.length - 1]) {
+      y.add(true);
+    }
+    q++;
+  }
+  if (y.length == list1.length) {
+    w = true;
+  } else
+    w = false;
+  return w;
+}
+
+void main() {
+  print(func(['hi', 're', 'do']));
+}
